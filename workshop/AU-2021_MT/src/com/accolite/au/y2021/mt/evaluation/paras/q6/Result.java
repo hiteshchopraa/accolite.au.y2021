eee2f0c5b285e10e85354a32792d2ea67ce91a38
Paras Tyagi <77529331+ParasTyagi17@users.noreply.github.com>
2021-05-24 00:19:42 +0530

    Update Result.java
    comments removed

--- workshop/AU-2021_MT/src/com/accolite/au/y2021/mt/evaluation/paras/q6/Result.java
+++ workshop/AU-2021_MT/src/com/accolite/au/y2021/mt/evaluation/paras/q6/Result.java
@@ -2,10 +2,10 @@ package com.accolite.au.y2021.mt.evaluation.paras.q6;
 
 import java.util.ArrayList;
 
-/*Result class is to store result of horses*/
+
 class Result{
-	ArrayList<Horse> list;//this will have horse objects 
-	int count=0;//this is counter for do while loop condition
+	ArrayList<Horse> list;
+	int count=0;
 	Result()
 	{
 		list=new ArrayList<>();
