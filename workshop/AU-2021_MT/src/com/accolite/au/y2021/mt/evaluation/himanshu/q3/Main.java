6495bbbfa505cd3dcd8197998557f459e4a2dd68
himanshu-kr-dev <himanshu.davasn@gmail.com>
2021-05-23 22:06:07 +0530

    Assignment Added (Ques3)

--- workshop/AU-2021_MT/src/com/accolite/au/y2021/mt/evaluation/himanshu/q3/Main.java
+++ workshop/AU-2021_MT/src/com/accolite/au/y2021/mt/evaluation/himanshu/q3/Main.java
@@ -5,19 +5,19 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 class Deadlock{
-	static final String r1 = "Resouce1";
-   	static final String r2 = "Resouce2";
+	static final String Resource1 = "Resouce1";
+   	static final String Resource2 = "Resouce2";
 	public static class Thread1 implements Callable<String>{
 	   @Override
 	   public String call() throws Exception {
-		   synchronized(r1){
-				System.out.println("Thread 1: Locked r1");
+		   synchronized(Resource1){
+				System.out.println("Thread 1: Locked Resource 1");
 				try{ 
 					Thread.sleep(100);
 				}
 				catch(Exception e) {}
-		  		synchronized(r2){
-					System.out.println("Thread 1: Locked r2");
+		  		synchronized(Resource2){
+					System.out.println("Thread 1: Locked Resource 2");
 				}
 		 	}
 		 	return "grbg";
@@ -27,14 +27,14 @@ class Deadlock{
 	public static class Thread2 implements Callable<String>{
 		@Override
 		public String call() throws Exception {
-			synchronized(r2){
-				System.out.println("Thread 2: Locked r2");
+			synchronized(Resource2){
+				System.out.println("Thread 2: Locked Resource 2");
 				try{ 
 					Thread.sleep(100);
 				} 
 				catch(Exception e) {}
-		   		synchronized(r1){
-		   			System.out.println("Thread 2: Locked r1");
+		   		synchronized(Resource1){
+		   			System.out.println("Thread 2: Locked Resource 1");
 		   		}
 		   }
 		   return "grbg";
@@ -48,5 +48,7 @@ public class Main{
 		ExecutorService ex = Executors.newFixedThreadPool(2);
 		Future<String> f1 = ex.submit(t1);
 		Future<String> f2 = ex.submit(t2);
+        System.out.println(f1);
+        System.out.println(f2);
 	}
 }
