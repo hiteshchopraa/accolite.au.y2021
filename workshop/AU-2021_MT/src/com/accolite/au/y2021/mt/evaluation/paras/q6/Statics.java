e7ef27daafc131f3a433da661dc2dc0b6dfca0e8
Paras Tyagi <77529331+ParasTyagi17@users.noreply.github.com>
2021-05-24 00:21:16 +0530

    Update Statics.java
    comments removed

--- workshop/AU-2021_MT/src/com/accolite/au/y2021/mt/evaluation/paras/q6/Statics.java
+++ workshop/AU-2021_MT/src/com/accolite/au/y2021/mt/evaluation/paras/q6/Statics.java
@@ -1,7 +1,7 @@
 package com.accolite.au.y2021.mt.evaluation.paras.q6;
-/*Statics Thread used for updating horse object every time horse gets a new speed*/
+
 class Statics extends Thread{
-	 Horse horse;// global reference for horse object
+	 Horse horse;
 	 String Tname;
 	 int gap;
 	 public Statics(Horse h,String Tname,int gap)
@@ -12,18 +12,18 @@ class Statics extends Thread{
 	 }
 	 public void run()
 	 {   
-	      /*synchronized block so that only one thread can manipulate them at a single time*/
-	      synchronized(this){horse.speeds+=horse.speed;//adding speed value in previous speeds
-	      horse.totallSpeedChanges+=1;//increment in total speed changes by horse
-	      horse.speedHistory+=horse.speed+"m/s->";//concatenating current speed in previous history speeds string
-	      horse.distanceCovered+=horse.speed*gap;//DISTANCE COVERED IS CALCULATED BY SPEED VALUE MULTIPLY BY 10 example: IF SPEED =20 THEN 200 METERS
+	      
+	      synchronized(this){horse.speeds+=horse.speed;
+	      horse.totallSpeedChanges+=1;
+	      horse.speedHistory+=horse.speed+"m/s->";
+	      horse.distanceCovered+=horse.speed*gap;
 	      horse.timeTaken+=1;
-	      horse.averageSpeed=horse.speeds/horse.totallSpeedChanges;//calculating average speed
+	      horse.averageSpeed=horse.speeds/horse.totallSpeedChanges;
 	     
 	      if(horse.topSpeed<horse.speed)
-	         horse.topSpeed=horse.speed;//top speed updating
+	         horse.topSpeed=horse.speed;
 	      if(horse.lowSpeed>horse.speed)
-		     horse.lowSpeed=horse.speed;// low speed updating
+		     horse.lowSpeed=horse.speed;
 	   }
 	 }
 }
