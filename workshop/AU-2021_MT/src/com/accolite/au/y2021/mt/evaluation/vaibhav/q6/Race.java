2c746e677a54940ce9e923ac68eeb014eda877d7
Vaibhav Chotani <46738856+VaibhavChotani@users.noreply.github.com>
2021-05-24 08:48:41 +0530

    Update Race.java

--- workshop/AU-2021_MT/src/com/accolite/au/y2021/mt/evaluation/vaibhav/q6/Race.java
+++ workshop/AU-2021_MT/src/com/accolite/au/y2021/mt/evaluation/vaibhav/q6/Race.java
@@ -5,9 +5,9 @@ import java.util.HashMap;
 import java.util.Map;
 
 class Race implements Runnable {
-    Horse reference; // Reference of Horse class
-    Outcome outcome; // Reference of Outcome class
-    int N; // Total number of horse input by the user
+    Horse reference;
+    Outcome outcome;
+    int N;
 
     Race(Horse reference, Outcome outcome, int N) {
         this.reference = reference;
@@ -20,30 +20,25 @@ class Race implements Runnable {
         do {
             synchronized (this) {
 
-                // if distance covered becomes equal to track distance the race ends
-                // totalDistance is the distance of the track
+                
                 if (reference.distanceCovered == reference.totalDistance) {
                     outcome.al.add(reference);
                     outcome.count++;
                     break;
                 }
-                // Changing the speed of the Horse every 5 seconds but can be set to any time.
+              
                 if (reference.timeTaken % 5 == 0) {
-                    // This thread is responsible for changing the speed.
+                    
                     RandomSpeed rs = new RandomSpeed(reference);
                     Thread th = new Thread(rs);
                     th.start();
 
                 }
-                // This thread is storing and updating all the information of the Horse like
-                // speed, time , distance, average speed , minspeed,maxspeed,speed history at
-                // each instance.
+                
                 Statistics sta = new Statistics(reference, Thread.currentThread().getName());
                 Thread thd = new Thread(sta);
                 thd.start();
-                // join() is used so that no other thread can manipulate if the some other
-                // thread
-                // is making changes.
+          
                 try {
                     thd.join();
                 } catch (Exception e) {
@@ -53,12 +48,11 @@ class Race implements Runnable {
             }
         } while (true);
 
-        // HashMap for storing the data and picking the winner on the bases of Average
-        // speed.
+    
         HashMap<Horse, Number> map = new HashMap<Horse, Number>();
-        // Till all the horses(user input) finishes the race
+       
         if (outcome.count == N) {
-            // Adding the data from the list to HashMap
+           
             for (int i = 0; i < outcome.al.size(); i++) {
                 if (!map.containsKey(outcome.al.get(i))) {
                     map.put(outcome.al.get(i), outcome.al.get(i).averageSpeed);
@@ -66,9 +60,9 @@ class Race implements Runnable {
 
             }
             int counter = 0;
-            // For formating the time and average speed to two decimal spaces.
+            
             DecimalFormat df = new DecimalFormat(".##");
-            // Printing the Winner and data of all the horses.
+           
             for (Map.Entry<Horse, Number> hashMap : map.entrySet()) {
                 if (counter == 0) {
                     System.out.println("---------- WINNER ---------- is " + hashMap.getKey().horseName);
